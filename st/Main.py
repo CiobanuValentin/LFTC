95cfead4dd60e6fff83897fd0e2f0f36aef52ca7
Ciobanu Valentin Eduard <valentin.ciobanu98@gmail.com>
2020-10-15 19:38:50 +0300

    ST with alphabetically BST and one single ST

--- st/Main.py
+++ st/Main.py
@@ -1,8 +1,10 @@
 from model.St import SymbolTable
 
-st = SymbolTable()
-st.add("if")
-st.add("else")
-st.add("ion")
-print(st.add("marcel"))
-print(st.get("marcel"))
+if __name__ == "__main__":
+
+    st = SymbolTable()
+    st.add("if")
+    st.add("else")
+    st.add("ion")
+    print(st.add("marcel"))
+    print(st.get("marcel"))
